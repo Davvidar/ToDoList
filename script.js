01f637573f8dc24ee3b97c46be8e2e06c28e78a0
Davvidar <daargente@gmail.com>
2023-12-14 15:31:59 +0100

    decription create

--- script.js
+++ script.js
@@ -8,119 +8,106 @@ const linteThrough = 'line-through'
 let id 
 let listArray
 
-
-
-
-
-
-//FECHA//
+// FECHA //
 const DATE = new Date()
 date.innerHTML = DATE.toLocaleDateString('es', { weekday: 'long', month: 'short', day: 'numeric' })
 
-
-
-
-
-//ADD TASKS//
-
-function addTask(task, id, realizado, eliminado) {
-
-    if (eliminado) { return }
+// ADD TASKS //
+function addTask(task, id, realizado, eliminado, descripcion) {
+    if (eliminado) { return; }
 
     const REALIZADO = realizado ? check : uncheck
     const LINE = realizado ? linteThrough : ''
 
-
     const liElement = `
-                <li id="liElement">
-                    <i class="far ${REALIZADO}" data="realizado" id='${id}'></i>
-                    <p class="text ${LINE}">${task}</p>
-                    <i class="fas fa-trash de" data="eliminado" id='${id}'></i>
-                </li>    
-                    `
-    list.insertAdjacentHTML("beforeend", liElement)
+        <li id="liElement">
+            <i class="far ${REALIZADO}" data="realizado" id='${id}'></i>
+            <p class="text ${LINE}" data-id="${id}">${task}</p>
+            <i class="fas fa-trash de" data="eliminado" id='${id}'></i>
+            <div class="dropdown">
+                <button class="dropbtn">Descripción</button>
+                <div class="dropdown-content">
+                    <p>${descripcion}</p>
+                </div>
+            </div>
+        </li>    
+    `;
+    list.insertAdjacentHTML("beforeend", liElement);
 }
 
 btnEnter.addEventListener('click', () => {
-
-    const task = input.value
+    const task = input.value;
+    const descripcion = "Descripción de la tarea"; // Puedes obtener la descripción de alguna manera
     if (task) {
-        addTask(task, id, false, false)
+        addTask(task, id, false, false, descripcion);
         listArray.push({
             nombre: task,
             id: id,
             realizado: false,
-            eliminado: false
-        })
-    localStorage.setItem('ToDo', JSON.stringify(listArray))
-    input.value = ""
-    id++
+            eliminado: false,
+            descripcion: descripcion
+        });
+        localStorage.setItem('ToDo', JSON.stringify(listArray));
+        input.value = "";
+        id++;
     }
-
-})
+});
 
 document.addEventListener('keyup', function (event) {
-
     if (event.key == 'Enter') {
-        const task = input.value
+        const task = input.value;
+        const descripcion = "Descripción de la tarea"; // Puedes obtener la descripción de alguna manera
         if (task) {
-            addTask(task, id, false, false)
+            addTask(task, id, false, false, descripcion);
             listArray.push({
                 nombre: task,
                 id: id,
                 realizado: false,
-                eliminado: false
-            })
-
-        localStorage.setItem('ToDo', JSON.stringify(listArray))
-        input.value = ""
-        id++  
+                eliminado: false,
+                descripcion: descripcion
+            });
+            localStorage.setItem('ToDo', JSON.stringify(listArray));
+            input.value = "";
+            id++;
         }
-        
-
     }
-})
+});
 
 function taskDone(element) {
-    element.classList.toggle(check)
-    element.classList.toggle(uncheck)
-    element.parentNode.querySelector('.text').classList.toggle(linteThrough)
-    listArray[element.id].realizado = listArray[element.id].realizado ?false : true
+    element.classList.toggle(check);
+    element.classList.toggle(uncheck);
+    element.parentNode.querySelector('.text').classList.toggle(linteThrough);
+    listArray[element.id].realizado = listArray[element.id].realizado ? false : true;
 }
 
 list.addEventListener('click', function (event) {
-    const element = event.target
-    const elementData = element.attributes.data.value
+    const element = event.target;
+    const elementData = element.attributes.data.value;
     if (elementData === 'realizado') {
-        taskDone(element)
+        taskDone(element);
+    } else if (elementData === 'eliminado') {
+        taskUndone(element);
     }
-    else if (elementData === 'eliminado') {
-        taskUndone(element)
-    }
-    localStorage.setItem('ToDo', JSON.stringify(listArray))
-
-})
-
-//REMOVE TASKS//
+    localStorage.setItem('ToDo', JSON.stringify(listArray));
+});
 
 function taskUndone(element) {
-    element.parentNode.parentNode.removeChild(element.parentNode)
-    listArray[element.id].eliminado = true
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    listArray[element.id].eliminado = true;
 }
 
-
-
-let data = localStorage.getItem('ToDo')
-if (data){
-    listArray = JSON.parse(data)
-    id = listArray.length
-    loadList(listArray)
+let data = localStorage.getItem('ToDo');
+if (data) {
+    listArray = JSON.parse(data);
+    id = listArray.length;
+    loadList(listArray);
 } else {
-    listArray = []
-    id = 0
+    listArray = [];
+    id = 0;
 }
-function loadList(DATA){
+
+function loadList(DATA) {
     DATA.forEach(function (i) {
-    addTask(i.nombre,i.id, i.realizado,i.eliminado)
-})
+        addTask(i.nombre, i.id, i.realizado, i.eliminado, i.descripcion);
+    });
 }
